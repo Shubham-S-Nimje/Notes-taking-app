b0890b2c9ac72376cd8010e430613ee6b022c525
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2023-07-26 14:14:44 +0530

    404 page added

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require("express");
-const bodyParser = require('body-parser')
-const path = require('path');
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
-const notesRouter = require('./routes/notes')
-const adminRouter = require('./routes/admin')
+const notesRouter = require("./routes/notes");
+const adminRouter = require("./routes/admin");
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(notesRouter);
-app.use('/admin',adminRouter);
+app.use("/admin", adminRouter);
 
 app.use("/", (req, res, next) => {
-  res.send("<h1>Hello world!..</h1>");
+  res.render("404", {
+    pageTitle: "Page not founr",
+    path: "/",
+  });
 });
 app.listen(3000);
